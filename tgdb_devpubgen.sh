79292944d2327a4ac51525e4b8dcd1eb7d49abc6
Philipp Ritter <2619327+pheelee@users.noreply.github.com>
2024-01-12 08:31:32 +0000

    add git user

--- tgdb_devpubgen.sh
+++ tgdb_devpubgen.sh
@@ -6,6 +6,9 @@ curl -s -o genres.json https://api.thegamesdb.net/v1/Genres?apikey=$API_KEY
 curl -s -o developers.json https://api.thegamesdb.net/v1/Developers?apikey=$API_KEY
 curl -s -o publishers.json https://api.thegamesdb.net/v1/Publishers?apikey=$API_KEY
 
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
+
 git add genres.json developers.json publishers.json
 git commit -m "update devpubgen at $(date)"
 git push origin main
