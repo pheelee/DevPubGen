bf4595c7cc2cbff0a506a5f2d47f1dbad107946a
Philipp Ritter <2619327+pheelee@users.noreply.github.com>
2026-01-26 15:19:21 +0100

    feat: include database-reduced in commit

--- tgdb_devpubgen.sh
+++ tgdb_devpubgen.sh
@@ -9,6 +9,6 @@ curl -s -o publishers.json https://api.thegamesdb.net/v1/Publishers?apikey=$API_
 git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Actions"
 
-git add genres.json developers.json publishers.json
+git add genres.json developers.json publishers.json database-reduced.json
 git commit -m "update devpubgen at $(date)"
 git push origin main
